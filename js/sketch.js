ca21bb07ddc0e5a13337ca5829ab0c75f41bc71b
jaydonfrazer <71885545+jaydonfrazer@users.noreply.github.com>
2023-01-10 12:20:19 +0000

    cool wave

--- js/sketch.js
+++ js/sketch.js
@@ -18,29 +18,26 @@ function setup() {
 function draw() {
     background(0);
     noStroke();
+    let length = 30;
     ////Create wave////
     while (theta < maxAngle + offset) {
-        // Sine wave 4
-        y = sin(theta - 0.6) * amplitude;
-        fill(0, 50, 0);
-        ellipse(x, y + height / 2, 16);
-        // Sine wave 3
-        y = sin(theta - 0.4) * amplitude;
-        fill(0, 100, 0);
-        ellipse(x, y + height / 2, 16);
-        // Sine wave 2
-        y = sin(theta - 0.2) * amplitude;
-        fill(0, 150, 0);
-        ellipse(x, y + height / 2, 16);
-        // Sine wave 1
-        y = sin(theta) * amplitude;
-        fill(0, 255, 0);
-        ellipse(x, y + height / 2, 16);
-        // Cosine wave
-        y = cos(theta) * amplitude;
-        fill(255, 255, 0);
-        ellipse(x, y + height / 2, 16);
+        // amplitude = sin(theta)
+        amplitude = ((theta-offset)/maxAngle) * (windowHeight/2);
 
+        // create Sine Wave
+        for (i = length; i > 0; i--) {
+            y = sin(theta - (i * 0.05)) * amplitude;
+            fill(0, 255/length * i, 0);
+            //ellipse(x, y + height / 2, 16);
+            square(x, height/2 + y, 16);
+        }
+        // create Cosine Wave
+        for (i = length; i > 0; i--) {
+            y = cos(theta - (i * 0.05)) * amplitude;
+            fill(255, 255 - (255/length * i), 0);
+            //ellipse(x, y + height / 2, 16);
+            square(x, height/2 + y, 16);
+        }
         theta += 0.2;
         x = ((theta - offset) / maxAngle) * windowWidth;
     }
