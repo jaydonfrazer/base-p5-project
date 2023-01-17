aa7b45cb4b2e680cd30325e54b385283ea317f3e
jaydonfrazer <71885545+jaydonfrazer@users.noreply.github.com>
2023-01-17 10:53:35 +0000

    yep

--- js/grid1.js
+++ js/grid1.js
@@ -1,7 +1,6 @@
 function setup() {
-    const CELL_SIZE = 160;
+    const CELL_SIZE = 80;
     createCanvas(800, 800);
-    //noStroke();
     stroke(0)
     strokeWeight(5)
     let x = 0;
@@ -10,15 +9,31 @@ function setup() {
     let yVariation = 0;
     let widthVariation = 0;
     let heightVariation = 0;
-    let colours = [color('#FF0000'),color('#00FF00'),color('#FFFC00'),color('#00FFFF'),color('#FF0000')];
+    let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 3];
+    let currentWidth = random(sizes);
+    let currentHeight = random(sizes);
+    let colours = [color('#FF0000'),color('#00FF00'),color('#FFFC00'),color('#00FFFF'),color('#FFC000'),color('#FFFFFF'),color('#63666A')];
     while(y < height){
         while(x < width){
             fill(random(colours));
-            rect(x+random(-xVariation, xVariation), y+random(-yVariation, yVariation),
-            CELL_SIZE + random(-widthVariation, widthVariation), CELL_SIZE + random(-heightVariation, heightVariation));
-            x+=CELL_SIZE
+            // rect(x+random(-xVariation, xVariation), y+random(-yVariation, yVariation),
+            // CELL_SIZE + random(-widthVariation, widthVariation), CELL_SIZE + random(-heightVariation, heightVariation));
+            rect(x, y, currentWidth, currentHeight);
+            x+= currentWidth;
+            let availableWidth = width - x;
+            currentWidth = random(sizes);
+            //correct cell size if going outside canvas
+            while(currentWidth > availableWidth){
+                currentWidth -= CELL_SIZE;
+            }
         }
-        y+=CELL_SIZE;
+        y+= currentHeight;
+        let availableHeight = height - y;
+        currentHeight = random(sizes);
+                    //correct cell size if going outside canvas
+        while(currentHeight > availableHeight){
+            currentHeight-=CELL_SIZE;
+        }
         x=0;
     }
 }
